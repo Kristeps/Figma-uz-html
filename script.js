1311660577a91f2971c7302d0bff6207d7661f03
Kristeps <kristaps.snepers@marupe.edu.lv>
2025-04-30 11:41:10 +0000

    Es pabeidzu, uztaisiju log in un sudzibu.

---
+++ script.js
@@ -0,0 +1,15 @@
+function alinga() {
+  let lingviens = document.getElementById("username").value;
+  let lingdivi = document.getElementById("password").value;
+  if (lingviens == "Kristaps" && lingdivi == "1234")
+  {
+    alert("Pieslēgšanās veiksmīga!");
+  }
+  else {
+    alert("Nepareizs lietotājvārds vai parole!");
+  }
+}
+
+function hahaha() {
+  alert("Paldies par sūdzību!");
+}
